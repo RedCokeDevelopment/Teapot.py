4fce744867086d45a50f47c898fb9d6c4386b5e5
ColaIan <colaian.yt@gmail.com>
2020-12-20 19:14:20 +0800

    Updated Database Collation

--- teapot/managers/database.py
+++ teapot/managers/database.py
@@ -30,7 +30,7 @@ def __init__():
             db=teapot.config.db_schema(),
             user=teapot.config.db_user(),
             passwd=teapot.config.db_password(),
-            charset='utf8',
+            charset='utf8mb4',
             use_unicode=True
         )
         return (database)
